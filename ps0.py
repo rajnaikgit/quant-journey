47dcea4a13e2c5a28e5b1b2b9c8c08e245eafdfc
rajnaikgit <rajnaik813@gmail.com>
2025-09-16 19:36:14 +0530

    Add solution for MIT 6.0001 PS0
    Python script that takes user input, calculates exponentiation and log base 2.

---
+++ ps0.py
@@ -0,0 +1,8 @@
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+z = x ** y
+a = numpy.log2(x)
+print("x raised to the power y is:", z)
+print("log(base2) of x is:", a)
